b091661425eb25eb52c8d45a06c3f26ea417f09d
Todd Rylaarsdam <todd@toddr.org>
2023-10-27 00:09:09 -0700

    fully working

--- src/pages/ChartPage.vue
+++ src/pages/ChartPage.vue
@@ -77,6 +77,7 @@ onMounted( async () => {
 
 <template>
 	<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+		<router-link style="z-index: 10000" to="/stage/choose_project"><button class="fixed bottom-5 right-5 mt-4 bg-sky-500 hover:bg-sky-600 text-white font-semibold py-2 px-4 rounded">Restart</button></router-link>
 		<div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
 		<div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10" style="min-width:75%;">
 			<div class="mx-auto">
